5547b55c4d94e4832f887fac41778a4f2cedada6
avarayr <7735415+avarayr@users.noreply.github.com>
2025-10-03 18:42:13 -0400

    add onepasswrd

--- lib/apps/1password.ts
+++ lib/apps/1password.ts
@@ -2,18 +2,15 @@ import { findPattern } from "@/lib/findPattern";
 import { FixedStatus, type AppMeta } from "../../types";
 
 export const OnePassword: AppMeta = {
-  icon: "https://cdn.brandfetch.io/ids0xxqhX-/w/800/h/800/theme/light/symbol.png?c=1bxid64Mup7aczewSAYMX&t=1668082116841",
+  icon: "https://cdn.brandfetch.io/ids0xxqhX-/w/128/h/128/theme/light/symbol.png?c=1bxid64Mup7aczewSAYMX&t=1668082116841",
   id: "one-password",
   friendlyName: "1Password",
   twitter: "1Password",
   async checkIsFixed() {
-    const url =
-      "https://downloads.1password.com/mac/1Password.zip";
+    const url = "https://downloads.1password.com/mac/1Password.zip";
     const pat = "_cornerMask";
     const result: { found: boolean; pos?: number } | null | undefined =
       await findPattern(url, pat);
     return result?.found ? FixedStatus.NOT_FIXED : FixedStatus.FIXED;
   },
 };
-
-
--- lib/apps/index.ts
+++ lib/apps/index.ts
@@ -3,11 +3,11 @@ import { Cluely } from "./cluely";
 import { Cursor } from "./cursor";
 import { Discord } from "./discord";
 import { DiscordCanary } from "./discord-canary";
-import { Figma } from './figma';
-import { FigmaBeta } from './figma-beta';
+import { Figma } from "./figma";
+import { FigmaBeta } from "./figma-beta";
 import { GitHubDesktop } from "./github-desktop";
 import { Notion } from "./notion";
-import { PocketCasts } from './pocket-casts'
+import { PocketCasts } from "./pocket-casts";
 import { Postman } from "./postman";
 import { Signal } from "./signal";
 import { Slack } from "./slack";
@@ -15,6 +15,8 @@ import { VisualStudioCode } from "./visual-studio-code";
 import { VisualStudioCodeInsiders } from "./visual-studio-code-insiders";
 import { Windsurf } from "./windsurf";
 import { Beeper } from "./beeper";
+import { OnePassword } from "./1password";
+
 import type { AppMeta } from "../../types";
 
 export const APPS: AppMeta[] = [
@@ -34,6 +36,7 @@ export const APPS: AppMeta[] = [
   VisualStudioCode,
   VisualStudioCodeInsiders,
   Windsurf,
+  OnePassword,
   Beeper,
 ];
 export default APPS;
