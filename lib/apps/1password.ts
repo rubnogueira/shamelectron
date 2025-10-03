089287a64958895a362729eb4513979e79a404b7
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2025-10-03 18:36:26 -0400

    Add 1Password

--- lib/apps/1password.ts
+++ lib/apps/1password.ts
@@ -1,17 +1,19 @@
 import { findPattern } from "@/lib/findPattern";
 import { FixedStatus, type AppMeta } from "../../types";
 
-export const PocketCasts: AppMeta = {
-  icon: "https://cdn.brandfetch.io/ids8HGa-I3/w/820/h/820/theme/dark/logo.png?c=1bxid64Mup7aczewSAYMX&t=1740032928003",
-  id: "pocket-casts",
-  friendlyName: "Pocket Casts",
-  twitter: "pocketcasts",
+export const OnePassword: AppMeta = {
+  icon: "",
+  id: "one-password",
+  friendlyName: "1Password",
+  twitter: "1Password",
   async checkIsFixed() {
     const url =
-      "https://pocketcasts.com/get/mac";
+      "https://cdn.brandfetch.io/ids0xxqhX-/w/800/h/800/theme/light/symbol.png?c=1bxid64Mup7aczewSAYMX&t=1668082116841";
     const pat = "_cornerMask";
     const result: { found: boolean; pos?: number } | null | undefined =
       await findPattern(url, pat);
     return result?.found ? FixedStatus.NOT_FIXED : FixedStatus.FIXED;
   },
 };
+
+
