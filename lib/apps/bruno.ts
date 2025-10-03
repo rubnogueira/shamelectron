cd8f74aca7d41a2e4a88cd14eae8806d03a1d7d3
Avarayr <7735415+avarayr@users.noreply.github.com>
2025-10-03 18:56:17 -0400

    fix #3

---
+++ lib/apps/bruno.ts
@@ -0,0 +1,25 @@
+import { findPattern } from "@/lib/findPattern";
+import { FixedStatus, type AppMeta } from "../../types";
+
+export const Bruno: AppMeta = {
+  icon: "https://raw.githubusercontent.com/usebruno/bruno/main/assets/images/logo-transparent.png",
+  id: "bruno-stable",
+  friendlyName: "Bruno",
+  twitter: "use_bruno",
+  async checkIsFixed() {
+    const url = await fetch(
+      "https://api.github.com/repos/usebruno/bruno/releases/latest"
+    )
+      .then((res) => res.json())
+      .then(
+        (data) =>
+          data.assets.find((asset: { name: string }) =>
+            asset.name.endsWith("arm64_mac.zip")
+          )?.browser_download_url
+      );
+    const pat = "_cornerMask";
+    const result: { found: boolean; pos?: number } | null | undefined =
+      await findPattern(url, pat);
+    return result?.found ? FixedStatus.NOT_FIXED : FixedStatus.FIXED;
+  },
+};
