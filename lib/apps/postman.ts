69ecd77279bb20bce42f2e7f3989abb9fb137a27
Ben Poole <ben@ldcvia.com>
2025-10-07 12:59:35 +0100

    Postman twitter handle (#16)

--- lib/apps/postman.ts
+++ lib/apps/postman.ts
@@ -5,7 +5,7 @@ export const Postman: AppMeta = {
   icon: "https://cdn.brandfetch.io/idrVtxty7B/w/128/h/128/theme/dark/icon.png?c=1bxid64Mup7aczewSAYMX&t=1667906412376",
   id: "postman-stable",
   friendlyName: "Postman",
-  twitter: "postman",
+  twitter: "getpostman",
   async checkIsFixed() {
     const url = "https://dl.pstmn.io/download/latest/osx_arm64";
     const pat = "_cornerMask";
