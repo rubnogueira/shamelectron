cc1c765a5aac997134439a822da8824548f86c57
Avarayr <7735415+avarayr@users.noreply.github.com>
2025-10-09 12:24:41 -0400

    chore: update dependencies and ESLint configuration; remove .eslintrc.json

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    ignores: ["lib/apps/index.ts", ".next/**/*", "out/**/*", "node_modules/**/*"],
+    extends: compat.extends("next"),
+}]);
