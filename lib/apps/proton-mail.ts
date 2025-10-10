164387abcd699cb3a908799ea7ce3ea7495c433c
Yura Samsoniuk <1129082+sigito@users.noreply.github.com>
2025-10-10 12:35:09 +0000

    Adeded ProtonMail (#24)
    Co-authored-by: Yurii Samsoniuk <yura@samsoniuk.com>

---
+++ lib/apps/proton-mail.ts
@@ -0,0 +1,15 @@
+import { FixedStatus, type AppMeta } from "@/types";
+import { findPattern } from "@/lib/findPattern";
+
+export const ProtonMail: AppMeta = {
+  icon: "http://pmecdn.protonweb.com/image-transformation/?s=c&image=image%2Fupload%2Fstatic%2Flogos%2Ficons%2Fmail_xxy4bg.svg",
+  id: "proton-mail",
+  friendlyName: "Proton Mail",
+  twitter: "ProtonPrivacy",
+  async checkIsFixed() {
+    const url = "https://proton.me/download/mail/macos/ProtonMail-desktop.dmg";
+    const pat = "_cornerMask";
+    const result = await findPattern(url, pat);
+    return result?.found ? FixedStatus.NOT_FIXED : FixedStatus.FIXED;
+  },
+};
