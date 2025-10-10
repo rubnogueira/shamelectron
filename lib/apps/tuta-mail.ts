285e0bf130be481293bb85a1c764ff0b5f057f5b
Yura Samsoniuk <1129082+sigito@users.noreply.github.com>
2025-10-10 12:34:11 +0000

    Added Tuta mail (#25)
    Co-authored-by: Yurii Samsoniuk <yura@samsoniuk.com>

---
+++ lib/apps/tuta-mail.ts
@@ -0,0 +1,14 @@
+import { FixedStatus, type AppMeta } from "@/types";
+import { findPattern } from "@/lib/findPattern";
+
+export const TutaMail: AppMeta = {
+  icon: "https://tuta.com/favicon/logo-favicon.svg",
+  id: "tuta-mail",
+  friendlyName: "Tuta Mail",
+  async checkIsFixed() {
+    const url = "https://app.tuta.com/desktop/tutanota-desktop-mac.dmg";
+    const pat = "_cornerMask";
+    const result = await findPattern(url, pat);
+    return result?.found ? FixedStatus.NOT_FIXED : FixedStatus.FIXED;
+  },
+};
