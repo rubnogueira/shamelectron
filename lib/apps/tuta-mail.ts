0ea8adbca144a1fef2ec4657dea1208127977d15
Paul <devnoname120@gmail.com>
2025-10-16 01:22:07 +0200

    Add Twitter for Tabby and Tuta Mail (#37)

--- lib/apps/tuta-mail.ts
+++ lib/apps/tuta-mail.ts
@@ -5,6 +5,7 @@ export const TutaMail: AppMeta = {
   icon: "https://tuta.com/favicon/logo-favicon.svg",
   id: "tuta-mail",
   friendlyName: "Tuta Mail",
+  twitter: "TutaPrivacy",
   async checkIsFixed() {
     const url = "https://app.tuta.com/desktop/tutanota-desktop-mac.dmg";
     const pat = "_cornerMask";
